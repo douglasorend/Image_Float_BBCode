3108cbf4acf075f87c5b12a3fbbeb74486d14540
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-18 20:31:20 -0500

    v1.0 - November 22th, 2014
    v1.0 - November 22th, 2014
    o Initial Release of the mod

---
+++ Subs-BBCode-Float.php
@@ -0,0 +1,67 @@
+<?php
+/**********************************************************************************
+* Subs-BBCode-Float.php
+***********************************************************************************
+***********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but      *
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY    *
+* or FITNESS FOR A PARTICULAR PURPOSE.                                            *
+**********************************************************************************/
+
+function BBCode_Float(&$bbc)
+{
+	$bbc[] = array(
+		'tag' => 'imgleft',
+		'type' => 'unparsed_content',
+		'content' => '<img src="$1" style="float:left; margin:15px;" {width}{height} />',
+		'validate' => create_function('&$tag, &$data, $disabled', '
+			$data = strtr($data, array(\'<br />\' => \'\'));
+			if (strpos($data, \'http://\') !== 0 && strpos($data, \'https://\') !== 0)
+				$data = \'http://\' . $data;
+		'),
+		'disabled_content' => '($1)',
+	);
+	$bbc[] = array(
+		'tag' => 'imgleft',
+		'type' => 'unparsed_content',
+		'parameters' => array(
+			'width' => array('optional' => true, 'value' => ' width="$1"', 'match' => '(\d+)'),
+			'height' => array('optional' => true, 'value' => ' height="$1"', 'match' => '(\d+)'),
+		),
+		'content' => '<img src="$1" style="float:left; margin:15px;" />',
+		'validate' => create_function('&$tag, &$data, $disabled', '
+			$data = strtr($data, array(\'<br />\' => \'\'));
+			if (strpos($data, \'http://\') !== 0 && strpos($data, \'https://\') !== 0)
+				$data = \'http://\' . $data;
+		'),
+		'disabled_content' => '($1)',
+	);
+	$bbc[] = array(
+		'tag' => 'imgright',
+		'type' => 'unparsed_content',
+		'content' => '<img src="$1" style="float:right; margin:15px;" {width}{height} />',
+		'validate' => create_function('&$tag, &$data, $disabled', '
+			$data = strtr($data, array(\'<br />\' => \'\'));
+			if (strpos($data, \'http://\') !== 0 && strpos($data, \'https://\') !== 0)
+				$data = \'http://\' . $data;
+		'),
+		'disabled_content' => '($1)',
+	);
+	$bbc[] = array(
+		'tag' => 'imgright',
+		'type' => 'unparsed_content',
+		'parameters' => array(
+			'width' => array('optional' => true, 'value' => ' width="$1"', 'match' => '(\d+)'),
+			'height' => array('optional' => true, 'value' => ' height="$1"', 'match' => '(\d+)'),
+		),
+		'content' => '<img src="$1" style="float:right; margin:15px;" />',
+		'validate' => create_function('&$tag, &$data, $disabled', '
+			$data = strtr($data, array(\'<br />\' => \'\'));
+			if (strpos($data, \'http://\') !== 0 && strpos($data, \'https://\') !== 0)
+				$data = \'http://\' . $data;
+		'),
+		'disabled_content' => '($1)',
+	);
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,31 @@
+[hr]
+[center][color=red][size=16pt][b]IMAGE FLOATING TO LEFT OR RIGHT BBCODE v1.0[/b][/size][/color]
+[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3983]Link to Mod[/url]
+[/center]
+[hr]
+
+[color=blue][b][size=12pt][u]Introduction[/u][/size][/b][/color]
+This modification adds two BBCodes to allow a user to float an image to the left or right of the text.
+
+The two BBCodes take the form of:
+[code]
+[imgleft width=x height=x]{url}[/imgleft]
+[imgright width=x height=x]{url}[/imgright]
+[/code]
+where {url} is the image URL and the width and height parameters being optional.
+
+[color=blue][b][size=12pt][u]Admin Settings[/u][/size][/b][/color]
+There are no admin settings to this mod.  To disable, you must uninstall this mod.
+
+[color=blue][b][size=12pt][u]Compatibility Notes[/u][/size][/b][/color]
+This mod was tested on SMF 2.0.9, but should work on SMF 2.0 and up.
+
+[color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
+[quote]
+v1.0 - November 22th, 2014
+o Initial Release of the mod
+[/quote]
+
+[hr]
+[url=http://creativecommons.org/licenses/by/3.0][img]http://i.creativecommons.org/l/by/3.0/80x15.png[/img][/url]
+This work is licensed under a [url=http://creativecommons.org/licenses/by/3.0]Creative Commons Attribution 3.0 Unported License[/url]
